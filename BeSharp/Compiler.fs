b7cf18c53f6dea6a3f95d62f5420a1e4397d24ff
Neil Danson <neil.danson@gmail.com>
2016-06-24 15:51:12 +0100

    Implement Equality
    Implement Equality. 
    Anding field equalities not working

--- BeSharp/Compiler.fs
+++ BeSharp/Compiler.fs
@@ -22,20 +22,49 @@ let resolveType typeName customTypes =
     | Custom(type') -> Some type'
     | _ -> None
 
+let getBestEquality (type':Type) = 
+    let specificEquals = type'.GetMethod("Equals", [|type'|])
+    if specificEquals <> null then   
+        specificEquals
+    else 
+        type'.GetMethod("Equals", [|typeof<obj>|])
+    
+
 let buildStruct (moduleBuilder:ModuleBuilder) name fields =
     let structType = moduleBuilder.DefineType(name, TypeAttributes.Public, typeof<System.ValueType>)
     structType, fun customTypes -> 
         let ctor = structType.DefineConstructor(MethodAttributes.Public ||| MethodAttributes.SpecialName ||| MethodAttributes.RTSpecialName, CallingConventions.HasThis, fields |> List.map snd |> List.choose (fun t -> resolveType t customTypes) |> List.toArray)
         
         let fields = fields |> List.map(fun (name, typeName) -> structType.DefineField(name, (resolveType typeName customTypes).Value, FieldAttributes.Public ||| FieldAttributes.InitOnly))
-        
+        let fieldsMap = fields |> List.map(fun f -> f.Name, f) |> Map.ofList
+
         let il = ctor.GetILGenerator()
         fields |> List.iteri(fun i field -> 
             il.Emit(OpCodes.Ldarg_0)
             il.Emit(OpCodes.Ldarg, i + 1)
             il.Emit(OpCodes.Stfld, field))
 
-        //TODO Equals/HashCode, etc
+        //TODO Lookup IEquatable and use?
+        let equalsMethod = structType.DefineMethod("Equals", MethodAttributes.Public ||| MethodAttributes.Virtual, typeof<bool>, [|typeof<obj>|])
+        let il = equalsMethod.GetILGenerator()
+        let other = il.DeclareLocal(structType)
+        il.Emit(OpCodes.Ldarg_1)
+        il.Emit(OpCodes.Castclass, structType)
+        il.Emit(OpCodes.Stloc, other)
+        il.Emit(OpCodes.Ldc_I4_1) //This does not work
+        il.Emit(OpCodes.Ldc_I4_1) //This does not work
+        fields |> List.iter (fun f -> 
+            let fieldEquals = getBestEquality f.FieldType
+            il.Emit(OpCodes.And) //This does not work
+            il.Emit(OpCodes.Ldarg_0)
+            il.Emit(OpCodes.Ldfld, f)
+            il.Emit(OpCodes.Ldloc, other)
+            il.Emit(OpCodes.Ldfld, f)
+            il.Emit(OpCodes.Call, fieldEquals)
+        )
+
+        il.Emit(OpCodes.Ldc_I4, 0)
+        il.Emit(OpCodes.Ret)
 
         structType.CreateType() |> ignore
 
