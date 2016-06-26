9a1b5d3a35ac3e6924f7d5c7e3149f3262aa4db3
Neil Danson <neil.danson@gmail.com>
2016-06-26 21:09:49 +0100

    Fixing up IL
    Still not done tho....

--- BeSharp.CompilerTests/TestCompiledOutput.cs
+++ BeSharp.CompilerTests/TestCompiledOutput.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace BeSharp.CompilerTests
 {
@@ -11,7 +6,7 @@ namespace BeSharp.CompilerTests
     {
         [Fact]
         public void TestEquality()
-        {
+        
             var item1 = new Hello(100, 200.0);
             var item2 = new Hello(100, 200.0);
             var equal = item1.Equals(item2);
