73a734118c1b7e146cc209095fe0b850c3dceb18
Neil Danson <Neil.Danson@simcorp.com>
2016-09-23 15:35:05 +0100

    Added 1st pass of type checker

---
+++ BeSharp.Tests/TypeCheckerTests.fs
@@ -0,0 +1,45 @@
+﻿module TypeCheckerTests
+
+open Xunit
+open AST
+open TypedAST
+open Parser
+
+let hasErrors results = results |> List.exists (function TypeCheckFail _ -> true | _ -> false)
+
+[<Fact>]
+let ``if cond must be boolean expression``() = 
+    let expr = TIf(TLiteral(Bool(true)), TLiteral(Int 1), TLiteral(Int 1))
+    let success =  checkExpr expr
+    Assert.Equal(false, hasErrors success)
+
+[<Fact>]
+let ``if cond must be boolean expression 2``() = 
+    let expr = TIf(TLiteral(Int(1)), TLiteral(Int 1), TLiteral(Int 1))
+    let result = checkExpr expr
+    Assert.Equal(true, hasErrors result)
+
+[<Fact>]
+let ``if branches must be of same type``() = 
+    let expr = TIf(TLiteral(Bool(false)), TLiteral(Int 1), TLiteral(Int 2))
+    let result = checkExpr expr
+    Assert.Equal(false, hasErrors result)
+
+[<Fact>]
+let ``if branches must be of same type 2``() = 
+    let expr = TIf(TLiteral(Bool(false)), TLiteral(Int 1), TLiteral(Bool false))
+    let result = checkExpr expr
+    Assert.Equal(true, hasErrors result)
+
+[<Fact>]
+let ``block must contain at least 1 expression``() = 
+    let expr = TBlock []
+    let result = checkExpr expr
+    Assert.Equal(true, hasErrors result)
+
+
+[<Fact>]
+let ``block must contain at least 1 expression 2``() = 
+    let expr = TBlock [TLiteral (Bool false)]
+    let result = checkExpr expr
+    Assert.Equal(false, hasErrors result)
--- BeSharp/TypedAST.fs
+++ BeSharp/TypedAST.fs
@@ -20,3 +20,23 @@ let rec getTypeOfExpr = function
 | TLet(_,_,expr) -> getTypeOfExpr expr
 | TBlock(exprs) -> getTypeOfExpr (exprs |> List.last)
 
+type TypeCheckResult = 
+| TypeCheckSuccess 
+| TypeCheckFail of string 
+
+let check f error= if f() then TypeCheckSuccess else TypeCheckFail error
+
+let checkIf expr trueExpr falseExpr =  
+    [ check (fun () -> getTypeOfExpr expr = typeof<bool>) "If Condition must equate to boolean" 
+      check (fun () -> (getTypeOfExpr trueExpr = getTypeOfExpr falseExpr)) "If Branches must have same return type" ]
+    
+let checkBlock exprs = 
+    [ check (fun () -> exprs |> List.exists (fun _ -> true)) "Block must contain at least 1 expression" ]
+
+let rec checkExpr = function 
+| TIf (cond,trueExpr,falseExpr) -> 
+    checkExpr cond @ checkExpr trueExpr @ checkExpr falseExpr @ checkIf cond trueExpr falseExpr
+| TBlock exprs -> checkBlock exprs
+| _ -> []
+
+
